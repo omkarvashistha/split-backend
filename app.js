27b905708b7fa97cdc9feaf6b0e91de00c2693bd
omkarvashistha <omkarvashistha90@gmail.com>
2024-03-22 15:32:55 +0530

    fixed not sending call error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require('./routes/mainRoutes');
 app.use(express.json());
 app.use('/',routes);
 
-const port = process.env.port || 5001;
+const port = process.env.PORT || 5001;
 
 app.listen(port,()=>{
   console.log(`Server started at http://localhost:${port}`)
